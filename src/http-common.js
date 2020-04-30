3a573dc87e24cd93f07f3156bb772d250c32014e
= <gonzalo.ricco1@gmail.com>
2020-04-29 21:55:00 -0300

    Change base url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://movies-api-challenge.herokuapp.com/api/",
   headers: {
     "Content-type": "application/json"
   }
